df8bf9eae709d8aa1872cddd86c67e0bd3e63bd2
ioddgamers <toluloperbrts@gmail.com>
2024-07-22 11:53:56 +0100

    Updated logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css"
 function Header() {
   return (
     <div>
-      <img src="/images/logo.svg" alt="Ping Logo" />
+      <img src="./images/logo.svg" alt="Ping Logo" />
         <h1>We are launching<span> soon!</span></h1>
         <p>subscribe and get notified</p>
     </div>
