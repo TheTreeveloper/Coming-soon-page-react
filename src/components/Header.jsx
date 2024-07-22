7ea4ac839a57effc464e9ea3cec1ab2f4e969fab
ioddgamers <toluloperbrts@gmail.com>
2024-07-22 11:53:33 +0100

    updated Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div>
       <img src="/images/logo.svg" alt="Ping Logo" />
-        <h1>We are launching<span>soon!</span></h1>
+        <h1>We are launching<span> soon!</span></h1>
         <p>subscribe and get notified</p>
     </div>
   )
