158493999206102c651f0b09f181649e045bb06b
ioddgamers <toluloperbrts@gmail.com>
2024-07-22 11:50:25 +0100

    Updated logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      <img src="/public/images/logo.svg" alt="Ping Logo" />
+      <img src="/images/logo.svg" alt="Ping Logo" />
         <h1>We are launching<span>soon!</span></h1>
         <p>subscribe and get notified</p>
     </div>
