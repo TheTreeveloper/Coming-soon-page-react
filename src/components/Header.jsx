e0def35b55de224c32a2683fbe3ad94b56b3a1d7
ioddgamers <toluloperbrts@gmail.com>
2024-07-22 11:48:30 +0100

    Added logo and text content

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <img src="/public/images/logo.svg" alt="Ping Logo" />
+        <h1>We are launching<span>soon!</span></h1>
+        <p>subscribe and get notified</p>
+    </div>
+  )
+}
+
+export default Header
